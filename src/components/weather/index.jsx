277976c6e4df6891e0ed640c880bf938153f0892
Austen Turner <austenpturner@msn.com>
2024-07-08 11:52:14 -0700

    functionality and styles from video

---
+++ src/components/weather/index.jsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+import Search from "../search";
+
+const apiKey = "3b4b13259a361de06a9426358092d38c";
+
+export default function Weather() {
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+
+  async function fetchWeatherData(city) {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+      );
+      const data = await response.json();
+      //   console.log(data);
+      if (data) {
+        setLoading(false);
+        setWeatherData(data);
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
+  function handleSearch() {
+    fetchWeatherData(search);
+  }
+
+  function getCurrentDate() {
+    return new Date().toLocaleDateString("en-us", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
+  }
+
+  useEffect(() => {
+    fetchWeatherData("Seattle");
+  }, []);
+
+  console.log(weatherData);
+
+  return (
+    <div>
+      <Search
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+      />
+      {loading ? (
+        <p className="loading">Loading... please wait</p>
+      ) : (
+        <div className="weather-data">
+          <div className="city-name">
+            <h2>
+              {weatherData?.name}, <span>{weatherData?.sys?.country}</span>
+            </h2>
+          </div>
+          <div className="date">
+            <span>{getCurrentDate()}</span>
+          </div>
+          <div className="temp">{weatherData?.main?.temp}</div>
+          <p className="description">
+            {weatherData && weatherData.weather && weatherData.weather[0]
+              ? weatherData.weather[0].description
+              : ""}
+          </p>
+          <div className="weather-info">
+            <div className="column">
+              <div>
+                <p className="wind">{weatherData?.wind?.speed}</p>
+                <p>wind speed</p>
+              </div>
+            </div>
+            <div className="column">
+              <div>
+                <p className="humidity">{weatherData?.main?.humidity}%</p>
+                <p>humidity</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
